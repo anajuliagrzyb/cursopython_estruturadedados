21c46202245e6f69ef7a4fc79f3e48075d94beeb
anajuliagrzyb <ana.grzyb@olxbr.com>
2023-06-15 15:41:42 -0300

    first commit

---
+++ manipulacao-de-variaveis.py
@@ -0,0 +1,28 @@
+#Manipulação de variaveis
+
+print('Variáveis inteiras')
+numero = -3
+numero_jogos = 14
+numero_convidados = 15
+print(numero, numero_jogos, numero_convidados)
+
+print('\n')
+
+print('Variáveis float')
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+print(pi)
+print(numero_euler)
+print(escala_terremoto)
+
+print('\n')
+
+print('Strings e char')
+letra = 'a'
+palavra1 = 'linguagem de programação'
+palavra2 = 'Python'
+print(letra, palavra1, palavra2)
+
+idade = input('Digite sua idade: ')
+print('Sua idade é: ', idade)
---
+++ operacoes-matematicas.py
@@ -0,0 +1,44 @@
+
+a = 5
+b = 3
+soma = a + b
+print(f'SOMA: {a} + {b} = {soma}')
+print(f'SUBTRAÇÃO: {a} - {b} = {a - b}')
+print(f'DIVISÃO: {a} / {b} = {a / b}')
+print(f'DIVISÃO EXATA: {a} // {b} = {a // b}')
+print(f'RESTO DA DIVISÃO: {a} % {b} = {a % b}')
+print(f'MULTIPLICAÇÃO: {a} X {b} = {a * b}')
+print(f'POTENCIAÇÃO: {a} ** {b} = {a ** b}')
+
+print('\n')
+print('*******************************')
+print('EXERCICIOS 1')
+
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+
+print(f'SOMA: {num1} + {num2} = {num1 + num2}')
+print(f'SUBTRAÇÃO: {num1} - {num2} = {num1 - num2}')
+print(f'DIVISÃO: {num1} / {num2} = {num1 / num2}')
+print(f'DIVISÃO EXATA: {num1} // {num2} = {num1 // num2}')
+print(f'RESTO DA DIVISÃO: {num1} % {num2} = {num1 % num2}')
+print(f'MULTIPLICAÇÃO: {num1} * {num2} = {num1 * num2}')
+print(f'POTENCIAÇÃO: {num1} ** {num2} = {num1 ** num2}')
+
+print('EXERCICIOS 2')
+
+tempo = float(input('Digite o tempo gasto no trajeto: '))
+velocidade = float(input('Digite a velocidade média: '))
+
+distancia = tempo * velocidade
+litros = distancia/12
+
+print('Tempo: ', tempo)
+print('Velocidade: ', velocidade)
+print('Distancia: ', distancia)
+print('Litros usados: %.f', litros)
+
+
+
+
+
